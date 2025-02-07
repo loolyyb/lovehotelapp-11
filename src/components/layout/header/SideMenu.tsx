321c3eb8d63f6a6d4192959eca3bbf7de2c25cfa
Philippe Zénone <philippe.zenone@free.fr>
2025-02-07 10:47:09 +0100

    Renomme lien conciergerie

--- src/components/layout/header/SideMenu.tsx
+++ src/components/layout/header/SideMenu.tsx
@@ -59,7 +59,7 @@ export function SideMenu() {
             onClick={() => setIsOpen(false)}
           >
             <Crown className="h-5 w-5" />
-            <span>Conciergerie</span>
+            <span>Conciergerie XXX</span>
           </Link>
           <Link
             to="/features"
