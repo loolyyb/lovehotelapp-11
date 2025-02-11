8363d9b2fbc59aff97b06cfd960112c2a7ed3a53
loolyyb <loolyyb@gmail.com>
2025-02-11 11:10:28 +0100

    Update SideMenu.tsx

--- src/components/layout/header/SideMenu.tsx
+++ src/components/layout/header/SideMenu.tsx
@@ -14,7 +14,7 @@ export function SideMenu() {
           <Menu className="h-5 w-5 text-burgundy" />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="w-64 bg-white/95 backdrop-blur-sm">
+      <SheetContent side="left" className="w-64 bg-white/10 backdrop-blur-sm">
         <img
           src="https://lovehotelaparis.fr/wp-content/uploads/2024/09/logo-web-love-hotel.png"
           alt="Love Hotel Logo"
